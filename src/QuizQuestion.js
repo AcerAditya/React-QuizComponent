4b9e9ba175fc93e1ab9e3f143e621c7fc251b08a
srivastava, aditya <aditya.srivastava@cvent.com>
2021-11-07 22:09:47 +0530

    dev

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,32 +1,21 @@
-import { isBindingIdentifier } from 'babel-types';
 import React, { Component } from 'react';
 
-let quizData = require('./quiz_data.json');
-
 class QuizQuestion extends Component {
-    constructor (props) {
-        super(props); 
-        this.state = {
-            quiz_position: 1,
-        };
-    }
 
     render() {
         return (
             <main>
-            <section>
-            <div>
-                <div className='QuizQuestion'>
-                    { quizData.quiz_questions[0].instruction_text }
-                </div>
-            </div>
-            </section>
-            <section className="buttons">
-              <ul>
-                //quiz question button logic goes here
-              </ul>
-            </section>
-          </main>
+                <section>
+                    <p>
+                        {this.props.qwer.instruction_text}
+                    </p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>{this.props.qwer.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
         );
     }
 }
