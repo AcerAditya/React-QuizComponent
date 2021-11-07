9361afbaf920ec41e4251eb2c0bfeafa30ce3b92
srivastava, aditya <aditya.srivastava@cvent.com>
2021-11-07 21:29:35 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Quiz from './QuizQuestion';
+import Quiz from './Quiz'
 class App extends Component {
   render() {
     return (
